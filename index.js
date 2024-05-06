854d6226c38cb4fce9a843f21c77af10346d389a
tui9999 <lolloroma2pun0suy@hotmail.com>
2024-05-07 00:08:39 +0700

    update aptos project

---
+++ index.js
@@ -0,0 +1,7 @@
+function canJump(nums) {
+  let lastPos = nums.length - 1;
+  for (let i = nums.length - 2; i >= 0; i--) {
+    if (i + nums[i] >= lastPos) lastPos = i;
+  }
+  return lastPos === 0;
+}
