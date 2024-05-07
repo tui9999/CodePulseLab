84229efe5c767bf890820296f61c5e1751b7f6c9
tui9999 <lolloroma2pun0suy@hotmail.com>
2024-05-07 11:58:17 +0700

    update commit

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-function canJump(nums) {
-  let lastPos = nums.length - 1;
-  for (let i = nums.length - 2; i >= 0; i--) {
-    if (i + nums[i] >= lastPos) lastPos = i;
+function minPathSum(grid) {
+  const rows = grid.length;
+  const cols = grid[0].length;
+  for (let i = 1; i < rows; i++) {
+    grid[i][0] += grid[i - 1][0];
   }
-  return lastPos === 0;
+  for (let j = 1; j < cols; j++) {
+    grid[0][j] += grid[0][j - 1];
+  }
+  for (let i = 1; i < rows; i++) {
+    for (let j = 1; j < cols; j++) {
+      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
+    }
+  }
+  return grid[rows - 1][cols - 1];
 }
