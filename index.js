0ebc5747c4424c28cb6b78515ea0f413610181ab
tui9999 <lolloroma2pun0suy@hotmail.com>
2024-05-08 12:55:14 +0700

    init project

--- index.js
+++ index.js
@@ -1,16 +1,5 @@
-function minPathSum(grid) {
-  const rows = grid.length;
-  const cols = grid[0].length;
-  for (let i = 1; i < rows; i++) {
-    grid[i][0] += grid[i - 1][0];
-  }
-  for (let j = 1; j < cols; j++) {
-    grid[0][j] += grid[0][j - 1];
-  }
-  for (let i = 1; i < rows; i++) {
-    for (let j = 1; j < cols; j++) {
-      grid[i][j] += Math.min(grid[i - 1][j], grid[i][j - 1]);
-    }
-  }
-  return grid[rows - 1][cols - 1];
+function sayHi() {
+  console.log("Hi!");
 }
+
+setTimeout(sayHi, 2000); // Run sayHi after 2 seconds
