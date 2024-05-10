0cd9ef53c2df4a2625eb1431c5c98cc2ec0bd7f6
tui9999 <lolloroma2pun0suy@hotmail.com>
2024-05-10 12:11:26 +0700

    demo sdk

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-function sayHi() {
-  console.log("Hi!");
+function buildTree(inorder, postorder) {
+  if (!inorder.length || !postorder.length) return null;
+  const rootVal = postorder[postorder.length - 1];
+  const root = new TreeNode(rootVal);
+  const mid = inorder.indexOf(rootVal);
+  root.left = buildTree(inorder.slice(0, mid), postorder.slice(0, mid));
+  root.right = buildTree(
+    inorder.slice(mid + 1),
+    postorder.slice(mid, postorder.length - 1),
+  );
+  return root;
 }
-
-setTimeout(sayHi, 2000); // Run sayHi after 2 seconds
